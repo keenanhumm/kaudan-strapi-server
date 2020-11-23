7a73c1e0f726edb38d129baf9f907000e017a34a
Keenan Humm <keenan.humm@gmail.com>
2020-11-23 14:29:21 -0600

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c1fd536e59ffd6fdf54af49803c0218d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '011b4ccf-6167-4fdc-8dc7-2289dbc651a8'
+};
